8f01e0505c6d09f7ee0ad97fec017838c234ebeb
Saroj Kumar Yadav <codeplus.np@gmail.com>
2024-09-15 17:55:13 +0545

    some unusual code removed

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -11,10 +11,9 @@
             <a href="/dashboard" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
         </li>
         <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="briefcase"></i><span>Master
-                    Entry</span></a>
+            <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="briefcase"></i><span>Master Entry</span></a>
             <ul class="dropdown-menu">
-                <li><a class="nav-link" href="#">Company Profile</a></li>
+                <li><a class="nav-link" href="#">Complete Profile</a></li>
                 <li><a class="nav-link" href="#">Carousel</a></li>
                 <li><a class="nav-link" href="#">Add Admin</a></li>
             </ul>
@@ -23,15 +22,6 @@
             <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="code"></i><span>Team</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="#">Add Developer</a></li>
-            </ul>
-        </li>
-        <li class="dropdown">
-            <a href="#" class="menu-toggle nav-link has-dropdown"><i
-                    data-feather="command"></i><span>Courses</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="#">Category</a></li>
-                <li><a class="nav-link" href="#">Course</a></li>
-                <li><a class="nav-link" href="#">Upcoming Classes</a></li>
             </ul>
         </li>
 
